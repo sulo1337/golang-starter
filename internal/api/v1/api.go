bfb37221f4ab519e7d73d86ea57b7daedb7cf079
sulo1337 <sulochan.acharya2@gmail.com>
2023-09-21 14:35:44 -0500

    feat: implement custom requestID middleware, use sensible chi middlewares

--- internal/api/v1/api.go
+++ internal/api/v1/api.go
@@ -1,6 +1,8 @@
 package api
 
 import (
+	chiMiddleware "github.com/go-chi/chi/v5/middleware"
+	"github.com/sulo1337/cleanarch-go/internal/api/middleware"
 	"github.com/sulo1337/cleanarch-go/pkg/logger"
 	"net/http"
 
@@ -26,6 +28,12 @@ func NewAPI(
 		userAPI: NewUserAPI(logger, userService),
 	}
 	r := chi.NewRouter()
+
+	r.Use(middleware.RequestID)
+	r.Use(chiMiddleware.CleanPath)
+	r.Use(chiMiddleware.RequestID)
+	r.Use(chiMiddleware.RealIP)
+	r.Use(chiMiddleware.Recoverer)
 
 	r.Route(prefix, func(r chi.Router) {
 		r.Mount("/users", api.userAPI.userRouter())
